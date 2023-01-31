fcae44a6b54169ee0988d54fccb03dcfdaf3739f
Ethan <eowin.saux@gmail.com>
2023-01-31 22:53:59 +0100

    [FIX] send message to reply

--- discordBot.py
+++ discordBot.py
@@ -17,9 +17,9 @@ class myBot(discord.Client):
             response = getResponse(message.content)
             if "choices" in response.keys():
                 for text in response["choices"]:
-                    await message.channel.send(text["text"], reference=message)
+                    await message.channel.reply(text["text"], mention_author=True)
             else:
-                await message.channel.send("Choices key missing, no output.", reference=message)
+                await message.channel.reply("Choices key missing, no output.", mention_author=True)
 
 intents = discord.Intents.default()
 intents.message_content = True
